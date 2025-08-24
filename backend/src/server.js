67d7c5ebbf58943731b9d5d6f2c5d96893eb8272
mhamzaanis <mhamza29216@example.com>
2025-08-24 18:07:49 +0500

    prep for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,16 +4,19 @@ import connectDB from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/ratelimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 // console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-
-app.use(cors(
-  {origin:"http://localhost:5173"}
-));
+if (!process.env.NODE_ENV !== "production"){
+  app.use(cors(
+    {origin:"http://localhost:5173"}
+  ));
+}
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter);
 
@@ -25,6 +28,14 @@ app.use((req, res, next)=> {
 
 //this lines means that any request to /api/notes will be handled by notesRoutes
 app.use("/api/notes", notesRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+  });
+}
 
 
 //once db is connected successfully only then hear on the port
